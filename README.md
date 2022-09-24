8af240a8ec44ae970c98ff1aae3de6969012e6d5
saikatnoman12 <noommaa240@gmail.com>
2022-09-24 14:14:24 +0600

    add readme file text

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # js_iterator
+
+js iterable:-
+ 1. Array
+ 2. String
+This is iterable because this is work permanent order. 
+But Object dose not work permanent order. 
+So Object is not iterable.
+But we can make object are iterable.
--- iterable.js
+++ iterable.js
@@ -1,14 +1,4 @@
-
-/*== 
-js iterable:-
- 1. Array
- 2. String
-This is iterable because this is work permanent order. 
-But Object dose not work permanent order. 
-So Object is not iterable.
-But we can make object are iterable.
-==*/
-
+// declare array
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 
 for (let i = 0; i < numbers.length; i++) {
